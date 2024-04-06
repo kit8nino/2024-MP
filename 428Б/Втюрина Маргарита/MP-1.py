6d76748b55c9eb7ce4180e90a0d305623d25c239
VtyurinaM <148870102+VtyurinaM@users.noreply.github.com>
2024-04-07 00:17:33 +0300

    Update MP-1.py

--- 428Б/Втюрина Маргарита/MP-1.py
+++ 428Б/Втюрина Маргарита/MP-1.py
@@ -16,7 +16,7 @@ atestat={'русский язык':5,
          'информатика':4,
          'латинский язык': 4,
          'финансовая грамотность':5}
-actor_birthday=datetime.date(1924,2,19)
+actor_birthday=datetime.datetime(1924,2,19,0,0,0)
 actor=("Lee","Marvin",(actor_birthday.day,actor_birthday.month,actor_birthday.year))
 men_names=['Иван',
              'Сергей',
@@ -82,8 +82,7 @@ def task1(atestat):
         x=atestat.get(i)
         sum+=x
     b=float(sum)/len(atestat)
-    print("{0:.2f}".format(b))
-
+    return b
 
 
 #задание 2
@@ -102,8 +101,9 @@ def task3(atestat):
     count=0
     for i in atestat.keys():
         for j in i:
-            count+=1
-    print(count)
+            if j!=' ':
+                count+=1
+    return count
 
 #задание 4
 def task4(atestat):
@@ -112,7 +112,7 @@ def task4(atestat):
         i=i.lower()
         for j in i:
             if j not in unique_letters and j!=' ': unique_letters.append(j)
-    print(unique_letters)
+    return unique_letters
 
 #задание 5
 def task5(tamandua):
@@ -121,20 +121,21 @@ def task5(tamandua):
         if i!=' ':
             bin=(format(ord(i),'08b'))
             str+=(bin)
-    print(str)
+    return str
+
 #задание 6
 def task6(actor_birthday):
     actual_date=datetime.datetime.now()
     time_difference=(actual_date-actor_birthday).days
-    print(f"Marvin Lee,  a famous actor,  was born {time_difference} days ago")
-
+    return time_difference
+    
 #задание 7
 def task7():
     import queue
     materials=queue.Queue()
-    print("Enter END to complete")
+    print("Введите END чтобы завершить ввод")
     def add_material(materials):
-        item=input("Enter the material: ")
+        item=input("Добавьте материал: ")
         if item=='END': 
             while materials.empty()==False:
                print(materials.get())
@@ -151,19 +152,20 @@ def task8(actor_birthday,men):
            
             
     number = (actor_birthday.day + actor_birthday.month**2 + actor_birthday.year) % 39 + 1
-    index=int(input(f"Enter the index to change someone's name (0-{len(men)-1})"))
-    if index<0 or index>9:print(f"You can enter an integer only in range (0-{len(men)-1})")
+    index=int(input(f"Введите индекс, чтобы поменять чьё-то имя (0-{len(men)-1}): "))
+    if index<0 or index>9:
+        print(f"Можно ввести индекс только в данном диапазоне (0-{len(men)-1})")
+        index=int(input(f"Введите индекс ещё раз, учитывая границы (0-{len(men)-1}): "))
     uniq=task2(men)
     uniq.sort(key=get_name)
-    print(number) #37 - Чжоу И-ван
+    print(f" Было выбрано имя императора под номером {number}") #37 - Чжоу И-ван
     uniq[index][0]='Чжоу'
     uniq[index][1]='И-ван'
     
-    print(uniq)
+    print (uniq)
 
 #задание 9
-def task9():
-    places={'Блювиничи': 1,
+places={'Блювиничи': 1,
             'Большое Бухалово': 2,
             'Свиновье': 3,
             'Синие Лепяги':4,
@@ -180,12 +182,14 @@ def task9():
             'Добрые Пчелы': 15,
             'Синегубово': 0
     }
-    places_list=['Блювиничи','Большое Бухалово','Свиновье','Синие Лепяги','Жабино','Кончинино','Раздериха','Чуваки','Мусорка','Голодранкино','Безводовка','Красная Могила','Кундрючья','Хотелово','Добрые Пчелы','Синегубово',]
+places_list=['Блювиничи','Большое Бухалово','Свиновье','Синие Лепяги','Жабино','Кончинино','Раздериха','Чуваки','Мусорка','Голодранкино','Безводовка','Красная Могила','Кундрючья','Хотелово','Добрые Пчелы','Синегубово',]
+def task9(places): 
+    print("Исходные данные:\n")
     print (places)
     
     
     def delete_place():
-        town=input('Enter the name, you want to delete ')
+        town=input('Введите город, который хотите удалить:  ')
         temp=[]
         for key in places.keys():
                     if key==town:break
@@ -196,11 +200,11 @@ def task9():
                 list0.append(x)
             places[list0[-1]]+=1
         else:places[temp[-1]]+=1
-        places[town]=None
+        print(f"\nСловарь, где {town} удален:\n")
         print(places)
-        
+        print("\n\n")
     def insert_konets(): 
-        position=int(input('Enter  the position'))
+        position=int(input('Введите позицию, на которую хотите вставить город "Конец": \n'))
         last=len(places)
         t='...'
         places.update({'Конец':position})
@@ -209,9 +213,23 @@ def task9():
                             t=key
         
         del places[t]
-        places.update({t:last}) 
+        places.update({t:last})
         sorted_tuple=sorted(places.items(),key=lambda x: x[1])
         places_new=dict(sorted_tuple)
+        print("\nОбновленный словарь: \n")
         print(places_new)
+        print("\n\n")
     delete_place()
     insert_konets()
+    
+print(" Средний балл в аттестате: {0:.2f}\n".format(task1(atestat)))
+print(f"Уникальный список имен: {task2(men)}\n")
+print(f" Общая длина всех названий предметов: {task3(atestat)}\n") 
+print(f"Уникальные буквы в названиях предметов: {task4(atestat)}\n")
+print(f"Имя тамандуа в бинарном виде: {task5(tamandua)}\n")
+print(f"Количество дней от даты рождения актера вестерна до текущей даты: {task6(actor_birthday)}\n")
+task7()
+print("\n")
+task8(actor_birthday,men)
+print("\n")
+task9(places)
