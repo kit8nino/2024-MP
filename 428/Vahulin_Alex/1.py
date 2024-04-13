aef3784920cce14c67ce98a9815b3ebe0a4121d3
AlexSol4R <rusmo2018@mail.ru>
2024-04-13 11:14:05 +0300

    Создал словарь из 14 учебных дисциплин с оценками

---
+++ 428/Vahulin_Alex/1.py
@@ -0,0 +1,16 @@
+subjects={
+    "Algebra": 5,
+    "Geometry": 4,
+    "Physics": 5,
+    "Biology": 3,
+    "Psychology": 4,
+    "Theology": 3,
+    "English": 5,
+    "Russian": 4,
+    "French": 3,
+    "Alchemy": 5,
+    "Demonology": 5,
+    "3D-modeling": 5,
+    "Radioelectronics": 5,
+    "Physical education": 5
+    }
