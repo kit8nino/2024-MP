ecdfcc16eede8863644c74260b438c73d300d9b4
SurFace81 <sur.f4ce@yandex.ru>
2024-04-13 18:40:56 +0300

    work2 started

---
+++ 429/Surin Vasiliy/work2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace work2
+{
+    internal class Program
+    {
+        const int birth_day = 28; 
+        const int birth_month = 1;
+        const string FILE_PATH = "D:\\Файлы\\ProgIng\\429\\Surin Vasiliy\\work2\\book.txt";
+
+        static void Main(string[] args)
+        {
+            int n = 999999;
+            RandomExt extRand = new RandomExt(100500);
+            int[] num1 = extRand.randShuffle(listGenerator(n));
+
+            double[] num2 = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                num2[i] = extRand.randRange(-1, 1);
+            }
+
+            Complex[] num3 = complexListGenerator(42000);
+
+            string[] words = splitFile(FILE_PATH);
+
+            // ------------------
+            printArr(extRand.randomlySelectedSort(), false);    // 8, 7, 2, 10
+        }
+
+        private static void printArr(int[] arr, bool newLine = true)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (newLine)
+                    Console.WriteLine(arr[i]);
+                else
+                    Console.Write(arr[i] + " ");
+            }
+
+            if (!newLine) Console.WriteLine();
+        }
+
+        private static string[] splitFile(string path)
+        {
+            string text = File.ReadAllText(path);
+            return Regex.Split(text, @"\W+");
+        }
+
+        private static int[] listGenerator(int n)
+        {
+            int[] res = new int[n + 1];
+            for (int i = 0; i <= n; i++)
+                res[i] = i;
+
+            return res;
+        }
+
+        private static Complex[] complexListGenerator(int n)
+        {
+            double r = birth_day / birth_month;
+            RandomExt extRand = new RandomExt();
+            Complex[] res = new Complex[n];
+            for (int i = 0; i < n; i++)
+            {
+                double x = extRand.randRange(-r, r);
+                double y = extRand.randRange(-r, r);
+                if (x * x + y * y < r)
+                {
+                    res[i] = new Complex(x, y);
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ 429/Surin Vasiliy/work2/SortingAlgorithms.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace work2
+{
+    internal class SortingAlgorithms
+    {
+        public int[] BubbleSort(int[] arr)
+        {
+            bool unsorted = true;
+            int r = arr.Length - 1;
+            while (unsorted)
+            {
+                unsorted = false;
+                for (int i = 0; i < r; i++)
+                {
+                    if (arr[i + 1] < arr[i])
+                    {
+                        (arr[i + 1], arr[i]) = (arr[i], arr[i + 1]);    // swap
+                        unsorted = true;
+                    }
+                }
+                r -= 1;
+            }
+
+            return arr;
+        }
+
+        public int[] GnomeSort(int[] arr)
+        {
+            int n = arr.Length;
+            if (n < 2) return arr;
+
+            int i = 1, j = 2;
+            while (i < n)
+            {
+                if (arr[i - 1] < arr[i])
+                {
+                    i = j;
+                    j = j + 1;
+                } 
+                else
+                {
+                    (arr[i - 1], arr[i]) = (arr[i], arr[i - 1]);
+                    i = i - 1;
+                    if (i == 0)
+                    {
+                        i = j;
+                        j = j + 1;
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+        public int[] SelectionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minInd = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[minInd])
+                    {
+                        minInd = j;
+                    }
+                }
+
+                (arr[i], arr[minInd]) = (arr[minInd], arr[i]);
+            }
+
+            return arr;
+        }
+
+        public int[] QuickSort(int[] arr, int left, int right)
+        {
+            if (arr.Length < 2) return arr;
+
+            if (left < right)
+            {
+                int pivotInd = (left + right) / 2;
+                int i = left;
+                int j = right;
+
+                while (i <= j)
+                {
+                    while (arr[i] < arr[pivotInd]) i++;
+                    while (arr[j] > arr[pivotInd]) j--;
+
+                    if (i <= j)
+                    {
+                        (arr[i], arr[j]) = (arr[j], arr[i]);
+                        i++;
+                        j--;
+                    }
+                }
+
+                QuickSort(arr, left, j);
+                QuickSort(arr, i, right);
+            }
+
+            return arr;
+        }
+    }
+}
