a5ddc1375af3aee84bb48d83f6b6bcffec3f841b
SweetRoll0818 <153400014+SweetRoll0818@users.noreply.github.com>
2024-04-30 23:33:38 +0300

    Update and rename first to first.py

---
+++ 429/Safronov Daniil/first.py
@@ -0,0 +1,23 @@
+#------------------------------------------
+a ={'английский':2,
+ 'русский':5,
+ 'математика':4,
+ 'phisika':3,
+ 'Nemeckiy':2,
+ 'Biologia':4,
+ 'Fizra': 1,
+ 'Matanaliz':5,
+ 'Algem':5,
+ 'Himia':1,
+ 'DockerFile':3,
+ 'Informatuka':2,
+ 'Okruzhayushiy mir':5,
+ 'OBZH':2,
+ 'History':5,
+ 'TFKP':5,
+ 'Labi':2}
+Sum = sum(a.values())
+Avg = Sum/len(a)
+print(Avg)
+    
+#---------------------------------------------
