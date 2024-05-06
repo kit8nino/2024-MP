166d81a57be5320efcd604a0eb26b404966480d7
Creepster <ibw2003@mail.ru>
2024-05-06 15:29:35 +0300

    [1] complete actions №1,№2,№3

--- 426/Воронин Иван/[1]/main.py
+++ 426/Воронин Иван/[1]/main.py
@@ -7,15 +7,15 @@ import random
 subject_list = {"memology", "astrology", "biology", "geology", "language", "pe", "soft development", "hard envelopment", "physics",
 	"thaumaturgy", "navigation", "sailing", "mailing", "stealth chatting", "party making"}
 
-sertificate = {}
+certificate = {}
 
 for subject in subject_list:
 	mark = random.randint(1, 6) + 1
 	if mark > 5:
-		sertificate[subject] = 5
+		certificate[subject] = 5
 	else:
-		sertificate[subject] = mark
-# int[] sertificate - random generated dictionary with marks (with correction to average about 4.5 and limited to [2; 5])
+		certificate[subject] = mark
+# int[] certificate - random generated dictionary with marks (with correction to average about 4.5 and limited to [2; 5])
 
 
 
@@ -38,5 +38,42 @@ for i in range(30):
 
 # №4
 tamandua_name = "long bear"
+
+
+
+# Actions:
+
+# №1
+summ = 0
+for i in certificate:
+	summ += certificate[i]
+average = summ / len(certificate)
+print(f"action №1 (average mark): {average}\n")
+
+
+
+# №2
+names_array_uniq = []
+counter2 = 0
+for i in range(len(names_array)):
+	counter = 0
+	for j in range(i+1, len(names_array)):
+		if names_array[i] == names_array[j]:
+			counter += 1
+			# print(f"name {names_array[i]} equal {names_array[j]}")
+	if counter == 0:
+		names_array_uniq.append(names_array[i])
+	else:
+		counter2 += 1
+print(f"action №2 (unique names):\n{names_array_uniq}\n\n{counter2} names are repeated and have been removed\n")
+
+
+
+# №3
+summ = 0
+for subject in certificate:
+	summ += len(subject)
+print(f"action №3 (total certificate lenght): {summ}")
+
 
 
