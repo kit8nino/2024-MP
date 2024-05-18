b77f9dc72750a7ac41ae72d0dec60535e0b80e25
takikamaru <58665878+takikamaru@users.noreply.github.com>
2024-05-18 13:17:37 +0300

    [2] update

--- 426/KrinochkinM/[2].py
+++ 426/KrinochkinM/[2].py
@@ -1,37 +1,35 @@
-# import random
-#
-# print(random.sample(range(1, 18), 4))
-
-# Result: [9, 2, 3, 11]
-
 import random
 import math
 
-# список целых чисел от 0 до 999999
-random_numbers = [random.randint(0, 999999) for i in range(1000000)]
+# Функция для генерации начальных данных
+def generate_data():
+    # список целых чисел от 0 до 999999
+    random_numbers = [random.randint(0, 999999) for i in range(1000000)]
 
-#список из 99999 случайных вещественных чисел в диапазоне [-1, 1];
-random_floats = [random.uniform(-1, 1) for i in range(100000)]
+    # список из 99999 случайных вещественных чисел в диапазоне [-1, 1]
+    random_floats = [random.uniform(-1, 1) for i in range(100000)]
 
-#42000 разных точки комплексной плоскости, лежащие внутри окружности радиуса r = birth_day / birth_month (можно случайных, можно равномерно распределённых), сортировать по модулю числа;
-# др 20.01
-random_points = []
-radius = 20
-for i in range(42000):
-    angle = random.uniform(0, 2 * math.pi)
-    random_radius = random.uniform(0, radius)
-    x = random_radius * math.cos(angle)
-    y = random_radius * math.sin(angle)
-    random_points.append(complex(x, y))
+    # 42000 разных точек комплексной плоскости, лежащие внутри окружности радиуса r = birth_day / birth_month
+    #др 20.01
+    random_points = []
+    radius = 20
+    for i in range(42000):
+        angle = random.uniform(0, 2 * math.pi)
+        random_radius = random.uniform(0, radius)
+        x = random_radius * math.cos(angle)
+        y = random_radius * math.sin(angle)
+        random_points.append(complex(x, y))
 
-#отрывок из книги (любой, на свой выбор) не менее 10000 слов, разбитый в список по словам.
-with open('file_for_[2].txt', 'r', encoding='utf-8') as file:
-    book_contents = file.read()
-word_list = book_contents.split()
+    # отрывок из книги (любой, на свой выбор) не менее 10000 слов, разбитый в список по словам
+    with open('D:/file_for_[2].txt', 'r', encoding='utf-8') as file:
+        book_contents = file.read()
+    word_list = book_contents.split()
+
+    return random_numbers, random_floats, random_points, word_list
 
 #############################################################################################################
-#Реализация алгоритма №2 Метод пузырьком
 
+# Алгоритм сортировки пузырьком
 def bubble_sort(arr, key=lambda x: x):
     n = len(arr)
     for i in range(n):
@@ -44,23 +42,9 @@ def bubble_sort(arr, key=lambda x: x):
             break
     return arr
 
-
-random_numbers = bubble_sort(random_numbers)
-print (random_numbers)
-
-random_floats = bubble_sort(random_floats)
-print(random_floats)
-
-random_points = bubble_sort(random_points, key = abs)
-print(random_points)
-
-word_list = bubble_sort(word_list)
-print(word_list)
-
 #############################################################################################################
 
-#Реализация алгоритма №3 сортировка расческой;
-
+# Алгоритм сортировки расческой
 def comb_sort(arr, key=lambda x: x):
     gap = len(arr)
     shrink = 1.3
@@ -81,22 +65,9 @@ def comb_sort(arr, key=lambda x: x):
 
     return arr
 
-random_numbers = comb_sort(random_numbers)
-print(random_numbers)
-
-random_floats = comb_sort(random_floats)
-print(random_floats)
-
-random_points = comb_sort(random_points, key = abs)
-print(random_points)
-
-word_list = comb_sort(word_list)
-print(word_list)
-
-
 #############################################################################################################
-#Реализация алгоритма №9 Heapsort, пирамидальная сортировка;
 
+# Алгоритм пирамидальной сортировки
 def heapify(arr, n, i, key=lambda x: x):
     largest = i
     left = 2 * i + 1
@@ -124,56 +95,96 @@ def heap_sort(arr, key=lambda x: x):
 
     return arr
 
-random_numbers = heap_sort(random_numbers)
-print(random_numbers)
-
-random_floats = heap_sort(random_floats)
-print(random_floats)
-
-random_points = heap_sort(random_points, key = abs)
-print(random_points)
-
-word_list = heap_sort(word_list)
-print(word_list)
-
 #############################################################################################################
-#Реализация алгоритма №11 Merge sort, сортировка слиянием;
 
-def merge_sort(arr, key=lambda x: x): 
-    if len(arr) <= 1: 
-        return arr 
-     
-    mid = len(arr) // 2 
-    left = merge_sort(arr[:mid], key=key) 
-    right = merge_sort(arr[mid:], key=key) 
- 
-    return merge(left, right, key=key) 
- 
-def merge(left, right, key=lambda x: x): 
-    result = [] 
-    i = j = 0 
- 
-    while i < len(left) and j < len(right): 
-        if key(left[i]) <= key(right[j]): 
-            result.append(left[i]) 
-            i += 1 
-        else: 
-            result.append(right[j]) 
-            j += 1 
- 
-    result.extend(left[i:]) 
-    result.extend(right[j:]) 
+# Алгоритм сортировки слиянием
+def merge_sort(arr, key=lambda x: x):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half, key)
+    right_half = merge_sort(right_half, key)
+
+    return merge(left_half, right_half, key)
+
+def merge(left, right, key=lambda x: x):
+    result = []
+    left_idx = right_idx = 0
+
+    while left_idx < len(left) and right_idx < len(right):
+        if key(left[left_idx]) <= key(right[right_idx]):
+            result.append(left[left_idx])
+            left_idx += 1
+        else:
+            result.append(right[right_idx])
+            right_idx += 1
+
+    result.extend(left[left_idx:])
+    result.extend(right[right_idx:])
+
     return result
 
+#############################################################################################################
 
-random_numbers = merge_sort(random_numbers)
-print(random_numbers)
+# Функция для выбора алгоритма сортировки
+def choose_sorting_algorithm():
+    while True:
+        print("Выберите алгоритм сортировки:")
+        print("1. Сортировка пузырьком")
+        print("2. Сортировка расческой")
+        print("3. Пирамидальная сортировка")
+        print("4. Сортировка слиянием")
+        choice = input("Введите номер алгоритма (1-4): ")
 
-random_floats = merge_sort(random_floats)
-print(random_floats)
+        if choice == "1":
+            return bubble_sort
+        elif choice == "2":
+            return comb_sort
+        elif choice == "3":
+            return heap_sort
+        elif choice == "4":
+            return merge_sort
+        else:
+            print("Неверный выбор. Попробуйте снова.")
 
-random_points = merge_sort(random_points, key = abs)
-print(random_points)
+# Функция для выбора данных для сортировки
+def choose_data_to_sort():
+    while True:
+        print("Выберите данные для сортировки:")
+        print("1. Список целых чисел")
+        print("2. Список вещественных чисел")
+        print("3. Список комплексных чисел")
+        print("4. Список слов из книги")
 
-word_list = merge_sort(word_list)
-print(word_list)
+        choice = input("Введите номер данных (1-4): ")
+
+        if choice == "1":
+            return random_numbers
+        elif choice == "2":
+            return random_floats
+        elif choice == "3":
+            return random_points
+        elif choice == "4":
+            return word_list
+        else:
+            print("Неверный выбор. Попробуйте снова.")
+
+# Генерация начальных данных
+random_numbers, random_floats, random_points, word_list = generate_data()
+
+# Выбор алгоритма сортировки
+sorting_algorithm = choose_sorting_algorithm()
+
+# Выбор данных для сортировки
+data_to_sort = choose_data_to_sort()
+
+# Сортировка выбранных данных
+sorted_data = sorting_algorithm(data_to_sort)
+
+# Вывод отсортированных данных
+print("Отсортированные данные:")
+print(sorted_data)
