28125acdef5627782e5aa202d9cdbe0bd8a106c2
EgorovIvan425 <150245846+EgorovIvan425@users.noreply.github.com>
2024-05-20 19:47:03 +0300

    Update 4 programm.py

--- 425/IvanEgorov/4 programm.py
+++ 425/IvanEgorov/4 programm.py
@@ -1,72 +1,37 @@
 import math
 
-# Определение класса для представления трехмерных векторов
-class Vector3D:
-    def __init__(self, x, y, z):
-        # Инициализация компонентов вектора
-        self.x = x
-        self.y = y
-        self.z = z
+def create_vector(x, y, z):
+    return {'x': x, 'y': y, 'z': z}
 
-    # Метод для сложения двух векторов
-    def __add__(self, other):
-        return Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
+def add_vectors(v1, v2):
+    return create_vector(v1['x'] + v2['x'], v1['y'] + v2['y'], v1['z'] + v2['z'])
 
-    # Метод для вычитания одного вектора из другого
-    def __sub__(self, other):
-        return Vector3D(self.x - other.x, self.y - other.y, self.z - other.z)
+def subtract_vectors(v1, v2):
+    return create_vector(v1['x'] - v2['x'], v1['y'] - v2['y'], v1['z'] - v2['z'])
 
-    # Метод для умножения вектора на скаляр
-    def __mul__(self, scalar):
-        return Vector3D(self.x * scalar, self.y * scalar, self.z * scalar)
+def multiply_vector(v, scalar):
+    return create_vector(v['x'] * scalar, v['y'] * scalar, v['z'] * scalar)
 
-    # Метод для вычисления расстояния до другого вектора
-    def distance_to(self, other):
-        return math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2 + (self.z - other.z) ** 2)
+def distance_between_vectors(v1, v2):
+    return math.sqrt((v1['x'] - v2['x'])**2 + (v1['y'] - v2['y'])**2 + (v1['z'] - v2['z'])**2)
 
-    # Метод для преобразования декартовых координат в сферические
-    def to_spherical(self):
-        r = math.sqrt(self.x**2 + self.y**2 + self.z**2)
-        theta = math.atan2(math.sqrt(self.x**2 + self.y**2), self.z)  # угол места
-        phi = math.atan2(self.y, self.x)  # азимут
-        return r, math.degrees(theta), math.degrees(phi)
+def to_spherical(v):
+    r = math.sqrt(v['x']**2 + v['y']**2 + v['z']**2)
+    theta = math.atan2(math.sqrt(v['x']**2 + v['y']**2), v['z'])  # угол места
+    phi = math.atan2(v['y'], v['x'])  # азимут
+    return r, math.degrees(theta), math.degrees(phi)
 
-# Реализация паттерна Singleton для класса Radar
-class SingletonMeta(type):
-    _instances = {}
+def get_object_coordinates(radar_position, object_position):
+    relative_position = subtract_vectors(object_position, radar_position)
+    return to_spherical(relative_position)
 
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
+def predict_object_coordinates(radar_position, object_position, object_velocity, time):
+    future_position = add_vectors(object_position, multiply_vector(object_velocity, time))
+    relative_position = subtract_vectors(future_position, radar_position)
+    return to_spherical(relative_position)
 
-# Класс радара, использующий паттерн Singleton
-class Radar(metaclass=SingletonMeta):
-    def __init__(self, position):
-        self.position = position
-
-    # Метод для получения сферических координат объекта
-    def get_object_coordinates(self, flying_object):
-        relative_position = flying_object.position - self.position
-        return relative_position.to_spherical()
-
-    # Метод для предсказания сферических координат объекта через время t
-    def predict_object_coordinates(self, flying_object, time):
-        future_position = flying_object.position + flying_object.velocity * time
-        relative_position = future_position - self.position
-        return relative_position.to_spherical()
-
-# Класс для представления летающих объектов
-class FlyingObject:
-    def __init__(self, position, velocity):
-        self.position = position
-        self.velocity = velocity
-
-# Главная функция программы
 def main():
-    radar_position = Vector3D(0, 0, 0)  # координаты радара
-    radar = Radar(radar_position)
+    radar_position = create_vector(0, 0, 0)  # координаты радара
 
     num_objects = int(input("Введите количество летающих объектов: "))
     flying_objects = []
@@ -74,19 +39,18 @@ def main():
     for i in range(num_objects):
         x, y, z = map(float, input(f"Введите координаты объекта {i + 1} (x y z): ").split())
         vx, vy, vz = map(float, input(f"Введите скорость объекта {i + 1} (vx vy vz): ").split())
-        flying_objects.append(FlyingObject(Vector3D(x, y, z), Vector3D(vx, vy, vz)))
+        flying_objects.append({'position': create_vector(x, y, z), 'velocity': create_vector(vx, vy, vz)})
 
     print("\nТекущие сферические координаты объектов относительно радара:")
     for i, obj in enumerate(flying_objects):
-        r, theta, phi = radar.get_object_coordinates(obj)
+        r, theta, phi = get_object_coordinates(radar_position, obj['position'])
         print(f"Объект {i + 1}: r={r:.2f} м, θ(угол)={theta:.2f}°, φ(азимут)={phi:.2f}°")
 
     time = float(input("\nВведите время в секундах: "))
     print("\nСферические координаты объектов относительно радара через это время:")
     for i, obj in enumerate(flying_objects):
-        r, theta, phi = radar.predict_object_coordinates(obj, time)
+        r, theta, phi = predict_object_coordinates(radar_position, obj['position'], obj['velocity'], time)
         print(f"Объект {i + 1}: r={r:.2f} м, θ(угол)={theta:.2f}°, φ(азимут)={phi:.2f}°")
-
 
 if __name__ == "__main__":
     main()
