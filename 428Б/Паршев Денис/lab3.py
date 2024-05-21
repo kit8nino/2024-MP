6a49fa00be21d27ceffa3ced9cdd52500a278b96
MeDenisz <166334567+MeDenisz@users.noreply.github.com>
2024-05-21 21:00:53 +0300

    добавил алгоритм

--- 428Б/Паршев Денис/lab3.py
+++ 428Б/Паршев Денис/lab3.py
@@ -33,3 +33,39 @@ def depth_first_search(start, end, maze):
                 paths[neighbor] = paths[current_pos] + [current_pos]
 
     return None
+#Алгоритм А*
+def calculate_heuristic(current, end):
+    return abs(current[0] - end[0]) + abs(current[1] - end[1])
+
+def a_star_search(start, end, maze):
+    open_list = [start]
+    closed_list = []
+    came_from = {}
+    g_score = {start: 0}
+    f_score = {start: calculate_heuristic(start, end)}
+
+    while open_list:
+        current = min(open_list, key=lambda x: f_score[x])
+        if current == end:
+            path = [end]
+            while current in came_from:
+                current = came_from[current]
+                path.append(current)
+            path.reverse()
+            return path
+
+        open_list.remove(current)
+        closed_list.append(current)
+
+        for neighbor in check_adjacent_cells(maze, current):
+            if neighbor in closed_list:
+                continue
+            tentative_g_score = g_score[current] + 1
+            if neighbor not in open_list or tentative_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = tentative_g_score
+                f_score[neighbor] = tentative_g_score + calculate_heuristic(neighbor, end)
+                if neighbor not in open_list:
+                    open_list.append(neighbor)
+
+    return open_list
