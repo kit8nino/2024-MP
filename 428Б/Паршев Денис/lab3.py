cc25ae0fef61696cdcd0726afc304a53ca3e5955
MeDenisz <166334567+MeDenisz@users.noreply.github.com>
2024-05-21 20:38:14 +0300

    Задаю начальные координаты

--- 428Б/Паршев Денис/lab3.py
+++ 428Б/Паршев Денис/lab3.py
@@ -1,1 +1,17 @@
+# получаем начальные координаты
+def get_coordinates(maze):
 
+    x_start = int(input('Avatar X coordinate: '))  
+    y_start = int(input('Avatar Y coordinate: '))  
+
+    x_key = int(input('Key X coordinate: '))  
+    y_key = int(input('Key Y coordinate: '))  
+    maze[x_key][y_key] = " "
+    start_position = (x_start, y_start)
+    key_position = (x_key, y_key)
+
+    for y in range(len(maze[0])):
+        if maze[len(maze) - 1][y] == " ":
+            exit_position = (len(maze) - 1, y)
+
+    return start_position, exit_position, key_position
