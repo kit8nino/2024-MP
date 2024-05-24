c61429118925762d2bd4637d2842c0955f4e134e
Ваганов Андрей <andrey.V2003@mail.ru>
2024-05-24 20:52:06 +0300

    [2] Подготовлены исходные данные

--- 426/Ваганов Андрей/[2].py
+++ 426/Ваганов Андрей/[2].py
@@ -2,3 +2,26 @@ import random
 
 print(random.sample(range(1, 18), 4)) # вернет список из 4 случайных значений в заданном диапазоне
 # 10, 3, 2, 7
+
+array_1 = [0]*99999
+for i in range(99999):
+	array_1[i] = (random.randint(0, 999999))
+
+array_2 = [0]*99999
+for i in range(99999):
+	array_2[i] = (random.uniform(-1, 1))
+
+array_3 = []
+r = 22
+x_list = []
+y_list = []
+for i in range(42000):
+	x = random.uniform(-r, r)
+	y = random.uniform(-(r**2-x**2)**0.5, (r**2-x**2)**0.5)
+	array_3.append([x, y])
+
+file = open("text.txt", 'r', encoding="utf-8")
+array_4 = []
+for line in file:
+	for word in line.split(' '):
+		array_4.append(word)
