457e7912c51068893a07c7d30f3520a8b51cbf29
Ваганов Андрей <andrey.V2003@mail.ru>
2024-05-24 22:08:01 +0300

    [2] добавлен quicksort

--- 426/Ваганов Андрей/[2].py
+++ 426/Ваганов Андрей/[2].py
@@ -1,6 +1,6 @@
 import random
 
-print(random.sample(range(1, 18), 4)) # вернет список из 4 случайных значений в заданном диапазоне
+# print(random.sample(range(1, 18), 4)) # вернет список из 4 случайных значений в заданном диапазоне
 # 10, 3, 2, 7
 
 array_1 = [0]*99999
@@ -25,3 +25,26 @@ array_4 = []
 for line in file:
 	for word in line.split(' '):
 		array_4.append(word)
+
+
+
+def quicksort(array):
+	if len(array) <= 1:
+		return array
+	central = (array[0] + array[-1]) / 2
+	left = []
+	mid = []
+	right = []
+	for i in range(len(array)):
+		if array[i] < central:
+			left.append(array[i])
+		elif array[i] == central:
+			mid.append(array[i])
+		else:
+			right.append(array[i])
+	left = quicksort(left)
+	right = quicksort(right)
+	return left + mid + right
+
+array_1 = quicksort(array_1)
+print("quicksort")
