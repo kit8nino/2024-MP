3ac09a6dfbf10d2d12a7ae49d8b87aac317bf816
BelyhA <150242440+TTorshe@users.noreply.github.com>
2024-05-25 20:51:55 +0300

    Update 3.py

--- 425/Белых Александр/3.py
+++ 425/Белых Александр/3.py
@@ -1,195 +1,186 @@
 import numpy as np
-from collections import deque
 
-my_maze = []
+def Maze_Reader():
+    y_max=0
+    maze=[]
 
-def maze_input(file_name,maze):
+    with open('maze-for-u.txt') as maze2:
+        for line in maze2:
+           maze.append(line.strip('\n'))
+        x_max, y_max = len(maze[0]), len(maze)
+    return x_max,y_max, maze
 
-    with open (file_name,'r', encoding="utf-8") as file_in_maze:
-        adding_line = file_in_maze.readline()
-        adding_line = adding_line[:-1]
-        while adding_line!='':
-            maze.append(list(adding_line))
-            adding_line = file_in_maze.readline()
-            adding_line = adding_line[:-1]
+max_x,max_y,maze=Maze_Reader()
+maze_list= list(maze)
+POSSIBLE_WAYS = ('N', 'S', 'W', 'E')
 
-maze_input('maze-for-u.txt',my_maze)
+def set_point(coord, sign):
+    global maze_list
+    maze_row = list(maze_list[coord[1]])
+    maze_row[coord[0]] = sign
+    maze_list[coord[1]] = ''.join(maze_row)
 
+def get_point(maze, row):
+    return [maze[row].find(" "), row]
 
-x_0, y_0 = 0, 2
+def is_coord_in_maze(maze, coord):
+    if coord[0]<0 or coord[0]>len(maze[0])-1:
+        return False
+    if coord[1]<0 or coord[1]>len(maze)-1:
+        return False
+    return True
 
-x, y = x_0, y_0 
-
-x_key, y_key = 1000, 1867
-
-x_exit, y_exit = 1079, 1918
-
-my_maze[x_key][y_key]='*'
-
-
-def is_key(x,y,maze):
-    return x==x_key and y==y_key
-
-def is_in_boundaries(x, y, maze):
-    return 0 <= x < len(maze) and 0 <= y < len(maze[0])
-
-def is_empty(x, y, maze):
-    return maze[x][y] == ' ' or maze[x][y]=='*' 
-
-def is_available(x, y, d, maze):
-    x_new = x + d[0]
-    y_new = y + d[1]
-    return is_in_boundaries(x_new, y_new, maze) and is_empty(x_new, y_new, maze)
-
-def neighbours_current_list(x, y, maze=my_maze):
-    directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
-    neighbours = []
-    for d in directions:
-        if is_available(x, y, d, maze):
-            neighbours.append((x + d[0], y + d[1]))
-    return neighbours
-
-def cell_selector(cells_to_visit):
-    min_cost_cell = min(cells_to_visit, key=cost_key)
-    return min_cost_cell
-
-def cost_key(cell):
-    return cell[2]
-
-def cost_deikstra(x, y, x_0=x_0, y_0=y_0, x_exit=x_exit, y_exit=y_exit):
-    g = abs(x - x_0) + abs(y - y_0)  
-    return g
-
-def path_draw_deikstra(maze,path):
-    for px, py in path:
-        maze[px][py] = '.'
-    return maze
-
-def add_to_visit_list_deikstra(list_of_neighbours, path, visited, cells_to_visit):
-    for nx, ny in list_of_neighbours:
-        if (nx, ny) in visited:
-            continue
-
-        new_path = path + [(nx, ny)]
-        new_g = cost_deikstra(nx, ny)
-
-        cells_to_visit.append((nx, ny, new_g, new_path))
-                
-    return cells_to_visit
-
-
-def deikstra(x_0, y_0, maze):
-    cells_to_visit = deque([(x_0, y_0, cost_deikstra(x_0, y_0), [])])  
-    visited = set()
-    
-    maze[x_0][y_0] = 's'
-
-    while cells_to_visit:
-        current_cell = cell_selector(cells_to_visit)
-        x, y, g, path = current_cell
-
-        if is_key(x, y, maze):
-            path_draw_deikstra(maze,path)
-            maze[x][y] = '*'
-            return True
-        
-        cells_to_visit.remove(current_cell)
-
-        visited.add((x, y))
-
-        list_of_neighbours = neighbours_current_list(x, y, maze)
-
-        cells_to_visit = add_to_visit_list_deikstra(list_of_neighbours, path, visited, cells_to_visit)
-
-    return False
-    
-
-cells_to_visit = []
-
-visited = set()
-    
-def is_exit(x, y, maze):
-    return x == x_exit and y == y_exit
-
-def is_empty_a_key(x, y, maze):
-    return maze[x][y] == ' ' or maze[x][y] == '*' or maze[x][y] == '.'
-
-def is_available_a_key(x, y, d, maze):
-    x_new = x + d[0]
-    y_new = y + d[1]
-    return is_in_boundaries(x_new, y_new, maze) and is_empty_a_key(x_new, y_new, maze)
-
-
-def cost_a_star(x, y, x_0=x_0, y_0=y_0, x_exit=x_exit, y_exit=y_exit):
-    g = abs(x - x_0) + abs(y - y_0)  
-    h = 2 * round(np.sqrt((x_exit - x) ** 2 + (y_exit - y) ** 2), 3)
-    f = g + h
-    return f
-
-def path_draw_a_key(maze,path):
-    for px, py in path:
-        maze[px][py] = ','
-    return maze
-
-def add_to_visit_list_a_key(list_of_neighbours, path, visited, path_length, max_path_length, cells_to_visit = cells_to_visit):
-    for nx, ny in list_of_neighbours:
-        if (nx, ny) in visited:
-            continue
-
-        new_path = path + [(nx, ny)]
-        new_cost = cost_a_star(nx, ny)
-        new_path_length = path_length + 1  
-
-        if new_path_length <= max_path_length: 
-            if (nx, ny, new_cost, new_path, new_path_length) not in cells_to_visit:
-                cells_to_visit.append((nx, ny, new_cost, new_path, new_path_length))
-                
-    return cells_to_visit
-
-def find_exit(x_0, y_0, maze, max_path_length, visited=visited, cells_to_visit=cells_to_visit):
-    cells_to_visit = [(x_0, y_0, cost_a_star(x_0, y_0), [], 0)]
-    visited = set()
-
-    while cells_to_visit:
-        current_cell = cell_selector(cells_to_visit)
-        x, y, _, path, path_length = current_cell
-
-        if is_exit(x, y, maze):
-            maze = path_draw_a_key(maze,path)
-            return True
-
-        cells_to_visit.remove(current_cell)
-        
-        visited.add((x, y))
-
-        list_of_neighbours = neighbours_current_list(x, y, maze)
-
-        cells_to_visit = add_to_visit_list_a_key(list_of_neighbours, path, visited, path_length, max_path_length, cells_to_visit = cells_to_visit)
-
+def is_coord_exit(coord):
+    if coord[1]>len(maze)-2:
+        return True
     return False
 
-def maze_output(file_name, maze):
-    
-    with open(file_name, 'w', encoding="utf-8") as file_out_maze:
-        
-        file_out_maze.write('\n'.join(''.join(row) for row in maze))
-    
-    
-def exist_way_to_key(key_is_exist):
-    if key_is_exist == True:
-        print("Путь к ключу найден!")
-    else:
-        print("Путь к ключу не найден!")
-        
-def exist_way_to_exit(exit_is_exist):
-    if exit_is_exist == True:
-        print("Путь к выходу найден!")
-    else:
-        print("Путь к выходу не найден!")
-        
+def is_coord_treasure(coord):
+    global treasure_is_here
+    if coord[1] == treasure_is_here[1] and\
+        coord[0] == treasure_is_here[0]:
+        return True
+    return False
 
-key_existence = deikstra(x_0, y_0, my_maze)
-exist_way_to_key(key_existence)
-exit_existence = find_exit(x_key, y_key, my_maze, max_path_length=3300)
-exist_way_to_exit(exit_existence)
-maze_output('maze-for-me-done.txt', my_maze)
+def is_path_clean(maze, coord):
+    if maze[coord[1]][coord[0]] == '#':
+        return False
+    return True
+
+def step(coord, direction):
+    if direction == 'N':
+         return step_N(coord)
+    elif direction == 'S':
+         return step_S(coord)
+    elif direction == 'E':
+         return step_E(coord)
+    elif direction == 'W':
+         return step_W(coord)
+
+def step_N(coord):
+    return [coord[0], coord[1]-1]
+
+
+def step_E(coord):
+    return [coord[0]+1, coord[1]]
+
+
+def step_S(coord):
+    return [coord[0], coord[1]+1]
+
+def step_W(coord):
+    return [coord[0]-1, coord[1]]
+
+
+def cut_way_back(direction):
+    """
+    Cut the opposite direction in possible ways to prevent stepping back
+    """
+    if direction == 'N':
+        return ('N', 'E', 'W')
+
+    if direction == 'S':
+        return ('S', 'E', 'W')
+
+    if direction == 'E':
+        return ('N', 'E', 'S')
+
+    if direction == 'W':
+        return ('N', 'S', 'W')
+
+def restore_path(coord):
+    global maze_list, path_to_exit
+
+    for node in path_to_exit:
+        set_point(coord, ',')
+
+        coord = step(coord, node)
+
+count_2l_ways = 0 
+def find_a_way(maze, coord, possible_ways):
+    global path_to_exit, current_path, count_2l_ways
+
+    if is_coord_exit(coord):
+        print('exit')
+        return
+
+    if is_coord_treasure(coord):
+        print('finding a new way')
+        path_to_exit = current_path.copy()
+        return
+
+    if len(current_path) > len(path_to_exit):
+        count_2l_ways+=1
+
+        return
+
+    for direction in possible_ways:
+        if is_path_clean(maze, step(coord, direction)):
+               current_path.append(direction)
+               find_a_way(maze, step(coord, direction), cut_way_back(direction))
+               current_path.pop()
+    return 
+
+past_way = [[-1 for j in range(max_x)] for i in range(max_y)] # инициализация
+
+def find_the_exit(treasure, end):
+
+    queue = []
+    queue.append(treasure)
+
+    past_way[treasure[1]][treasure[0]] = 0 
+
+    while queue:
+        node = queue.pop(0)
+
+        for i in [[-1, 0],[1, 0],[0, -1],[0, 1]]:
+            x = node[0] + i[0]
+            y = node[1] + i[1]
+            if (x < 0 or x >= max_x or y < 0 or y >= max_y):
+                continue
+            if (maze[y][x] == " " and past_way[y][x] == -1):
+                past_way[y][x] = [node[0],node[1]]
+                queue.append([x, y]) 
+
+    x = end[0]
+    y = end[1] 
+    while past_way[y][x] != 0:
+        set_point([x,y], ',')
+        temp_x = past_way[y][x][0]
+        temp_y = past_way[y][x][1]
+        x, y = temp_x, temp_y
+        #print(past_way[y][x])
+
+path_to_exit = []
+for i in range(len(maze)*len(maze[0])):
+    path_to_exit.append(' ')
+current_path = []
+
+start_point = get_point(maze, 0)
+end_point = get_point(maze, max_y-1)
+print("start:", start_point)
+print("end:",end_point)
+
+treasure_is_here = ()
+
+while(not (treasure_is_here and is_path_clean(maze, treasure_is_here))):
+    x = np.random.randint(40,85) #int(input(f'Where is threasure? (x is between 0 and {max_x-1}) '))
+    y = np.random.randint(90,190) #int(input(f'Where is threasure? (y is between 0 and {max_y-1}) '))
+    treasure_is_here = (abs(x) if abs(x) < max_x-1 else max_x-1, abs(y) if abs(y) < max_y-1 else max_y-1)
+
+set_point(treasure_is_here, '*')
+print("treasure is here:",treasure_is_here)
+
+find_a_way(maze, start_point, POSSIBLE_WAYS)
+
+restore_path(start_point)
+
+find_the_exit(treasure_is_here, end_point)
+
+f = open('maze-for-me-done.txt', 'w')
+for i in range(len(maze_list)):
+    f.write(maze_list[i])
+    f.write("\n")
+f.close()
+print('The quontity of too long paths that we cut off: ', count_2l_ways) 
+print("Done!")
