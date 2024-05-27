1fb4c072f719a3ae99dcb8ce59af9d78feb15d5b
sergorudg <169088023+sergorudg@users.noreply.github.com>
2024-05-27 07:37:01 +0300

    Update 2.py
    Added radix sort

--- 428/Makarov Sergei/2.py
+++ 428/Makarov Sergei/2.py
@@ -1,5 +1,6 @@
 #Сортировка пузырьком
 import random
+import math
 L = []
 for x in range(1, 1000):
     ch = random.randint(1, 999999)
@@ -51,4 +52,45 @@ def gnome_sort(words):
             i += 1
     return words
 sorted_words = gnome_sort(words)
+
 #Поразрядная сортировка
+
+r = 31 / 8  
+points = []
+for _ in range(42000):
+    x = random.uniform(-r, r)
+    y = random.uniform(-r, r)
+    while x*2 + y*2 > r*2:
+        x = random.uniform(-r, r)
+        y = random.uniform(-r, r)
+    points.append(complex(x, y))
+def radix_sort(points):
+    num_digits = int(math.log10(max(abs(p.real) for p in points))) + 1
+    for digit in range(num_digits):
+        counting_sort(points, digit)
+    return points
+def counting_sort(points, digit):
+    counts = [0] * 10
+    for point in points:
+        index = get_digit(point, digit)
+        counts[index] += 1
+    for i in range(1, 10):
+        counts[i] += counts[i-1]
+    sorted_points = [None] * len(points)
+    
+    for point in reversed(points):
+        index = get_digit(point, digit)
+        sorted_points[counts[index] - 1] = point
+        counts[index] -= 1
+    for i in range(len(points)):
+        points[i] = sorted_points[i]
+
+def get_digit(point, digit):
+    num_str = f"{abs(point):.0f}"
+   
+    if len(num_str) >= digit + 1:
+        return int(num_str[-digit - 1])
+    else:
+        return 0
+sorted_points = radix_sort(points)
+print(sorted_points)
