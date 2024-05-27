fe97de3a5c34df39a3b0723d31e3f775e215fb23
sergorudg <169088023+sergorudg@users.noreply.github.com>
2024-05-27 06:56:10 +0300

    Update 2.py
    Added bubble sorting

--- 428/Makarov Sergei/2.py
+++ 428/Makarov Sergei/2.py
@@ -1,1 +1,18 @@
-123
+import random
+L = []
+for x in range(1, 1000):
+    ch = random.randint(1, 999999)
+    L.append(ch)
+    
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+print(bubble_sort(L))
