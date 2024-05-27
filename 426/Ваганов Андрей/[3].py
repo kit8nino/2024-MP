b3560dbb899ed25182525961ff38e35941f05170
Ваганов Андрей <andrey.V2003@mail.ru>
2024-05-27 03:43:19 +0300

    [3] завершена 3 лаба

--- 426/Ваганов Андрей/[3].py
+++ 426/Ваганов Андрей/[3].py
@@ -8,7 +8,7 @@ file.close()
 
 
 # Создание графа:
-class point():
+class Point():
 	def __init__(self, x, y, weigh = math.inf):
 		self.x = x
 		self.y = y
@@ -21,7 +21,7 @@ def makeGraph(map):
 	for y in range(1, len(map)-1):
 		for x in range(1, len(map[0])-1):
 			if map[y][x] != "#":
-				graph[x, y] = point(x, y)
+				graph[x, y] = Point(x, y)
 
 	# Задание соседей вершинам графа
 	for coords, point in graph.items():
@@ -36,17 +36,38 @@ def makeGraph(map):
 			point.neighbour.add(graph[x, y-1])
 	return(graph)
 
-
+def deijksrta(graph, pointA, pointB):
+	unchecked = set()
+	unchecked.add(pointA)
+	pointA.weigh = 0
+	addition = set()
+	limit = 1000
+	i = 0
+	while(i < limit):
+		if pointB in unchecked:
+			print("Точка найдена...")
+			break
+		for unchecked_vertex in unchecked:
+			for next_vertex in unchecked_vertex.neighbour:
+				addition.add(next_vertex)
+				if next_vertex.weigh > unchecked_vertex.weigh+1:
+					next_vertex.weigh = unchecked_vertex.weigh+1
+		unchecked.clear()
+		unchecked.update(addition)
+	print("Создание пути...")
+	path = []
+	point = pointB
+	while(point != pointA):
+		path.append([point.x, point.y])
+		neighbours = point.neighbour
+		for neighbour in neighbours:
+			if neighbour.weigh < point.weigh:
+				point = neighbour
+	return(path)
 
 start = [132, 45]
-# start[0] = int(input("Введите начальную координату (x): "))
-# start[1] = int(input("Введите начальную координату (y): "))
 key = [160, 70]
-# key[0] = int(input("Введите координату ключа (x): "))
-# key[1] = int(input("Введите координату ключа (y): "))
 exit = [100, 3]
-# exit[0] = int(input("Введите координату выхода (x): "))
-# exit[1] = int(input("Введите координату выхода (y): "))
 
 x, y = start[0], start[1]
 while map[start[0]][start[1]] != ' ':
@@ -77,3 +98,25 @@ while map[exit[0]][exit[1]] != ' ':
 
 print("Начало установлено на", str(start), "ключ на", str(key), "выход на", str(exit))
 
+graph1 = makeGraph(map)
+
+path1 = deijksrta(graph1, graph1[start[0], start[1]], graph1[key[0], key[1]])
+
+
+graph2 = makeGraph(map)
+
+path2 = deijksrta(graph2, graph2[key[0], key[1]], graph2[exit[0], exit[1]])
+
+# Запись пути в файл
+for coord in path1:
+	map[coord[1]][coord[0]] = '.'
+for coord in path2:
+	map[coord[1]][coord[0]] = ','
+
+map[x][y] = '*'
+file = open("maze-for-me-done.txt", 'w')
+for line in map:
+	for symbol in line:
+		file.write(symbol)
+	file.write('\n')
+file.close()
